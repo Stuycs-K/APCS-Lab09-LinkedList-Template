5abaa39f588c30eb3a1e5924f7c15b57dff42728
konstantinnovation <konstans@stuy.edu>
2023-01-12 14:49:37 -0500

    Update MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -9,19 +9,29 @@ public class MyLinkedList{
   public int size(){
     //Returns the number of elements in this list.
   }
-  public void add(int index, String element){
-    //Inserts the specified element at the specified position in this list.
-  }
-  
   public String get(int index){
     //Return the value at the specified index.
-    return true;
   }
   
   public boolean add(String e){
     //Appends the specified element to the end of this list.
     return true;
   }
+
+  public String toString(){
+    //same format as ArrayList "[A, B, C]" or "[]"
+    return "";
+  }
+
+   public void add(int index, String element){
+    //Inserts the specified element at the specified position in this list.
+  }
+  
+   public String set(int index, String value){
+    //Replaces the value at the specified index in this list with the specified value.
+    //Return the original String that was present
+  } 
+  
   public boolean contains(String element){
     //Returns true if this list contains the specified element, false otherwise.
   }
@@ -34,8 +44,5 @@ public class MyLinkedList{
     //Removes the first occurrence of the specified element from this list, if it is present..
   }
   
-  public String set(int index, String value){
-    //Replaces the value at the specified index in this list with the specified value.
-    //Return the original String that was present
-  } 
+
 }
