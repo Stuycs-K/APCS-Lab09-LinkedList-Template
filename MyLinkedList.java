2c4bd7d9ca6b75c373aa5400456b0c6c0ef7ae26
konstantinnovation <konstans@stuy.edu>
2023-01-12 12:48:02 -0500

    Create MyLinkedList.java

---
+++ MyLinkedList.java
@@ -0,0 +1,41 @@
+public class MyLinkedList{
+  private int size;
+  private ListNode front,back;
+
+  //You will only write the no argument Constructor, as you do not have a capacity.
+  public MyLinkedList(){
+  }
+  
+  public int 	size(){
+    //Returns the number of elements in this list.
+  }
+  public void add(int index, String element){
+    //Inserts the specified element at the specified position in this list.
+  }
+  
+  public String get(int index){
+    //Return the value at the specified index.
+    return true;
+  }
+  
+  public boolean add(String e){
+    //Appends the specified element to the end of this list.
+    return true;
+  }
+  public boolean contains(String element){
+    //Returns true if this list contains the specified element, false otherwise.
+  }
+  
+  public String remove(int index){
+    //Removes the element at the specified position in this list..
+  }
+  
+  public boolean remove(String element){
+    //Removes the first occurrence of the specified element from this list, if it is present..
+  }
+  
+  public String set(int index, String value){
+    //Replaces the value at the specified index in this list with the specified value.
+    //Return the original String that was present
+  } 
+}
